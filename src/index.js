2aec4091acddae90961c3947b2467436b32d768a
Laura Meikle <laura.s.meikle@gmail.com>
2018-07-03 22:03:40 +0100

    add dev tools

--- src/index.js
+++ src/index.js
@@ -8,24 +8,25 @@ import App from './app/App';
 import registerServiceWorker from './registerServiceWorker';
 import { banking, bankingSagas } from './banking/';
 
-//import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
-//import { REDUX_DEVTOOLS_SETTINGS } from '../constants';
-//const composeEnhancers = composeWithDevTools(REDUX_DEVTOOLS_SETTINGS);
-//const middleware = [thunkMiddleware]; // lets us dispatch() functions
-//if (process.env.NODE_ENV !== 'production')
-//{
-//	middleware.push(require('redux-logger').createLogger()); // neat middleware that logs actions
-//}
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
+const composeEnhancers = composeWithDevTools({
+  export: true
+});
 
 const sagaMiddleware = createSagaMiddleware();
 const middleware = [sagaMiddleware]; // lets us dispatch() functions
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(require('redux-diff-logger'));
+  middleware.push(require('redux-logger').createLogger());
+  const { whyDidYouUpdate } = require('why-did-you-update');
+  whyDidYouUpdate(React);
+}
 
 const store = createStore(
   combineReducers({
     banking
   }),
-  applyMiddleware(...middleware)
-  // composeEnhancers(applyMiddleware(...middleware)),
+  composeEnhancers(applyMiddleware(...middleware))
 );
 
 sagaMiddleware.run(bankingSagas);
