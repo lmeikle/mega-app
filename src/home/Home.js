7126ddfce892f529d26cfd10d706efeaeb5c1a0a
lmeikle <laura.s.meikle@gmail.com>
2018-07-04 15:10:25 +0100

    trying styled components

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
-import HomeTileComponent from './HomeTileComponent';
-import './Home.css';
+import styled from 'styled-components';
+import HomeTile from './HomeTile';
+
+const HomeTitle = styled.div`
+  font-weight: bold;
+  font-size: 20px;
+  padding: 20px 0;
+`;
+
+const HomeTilesWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 class Home extends Component {
   render() {
     return (
       <div className="home">
-        <div className="home-title">What would you like to do today?</div>
-        <div className="home-tiles-container">
-          <HomeTileComponent path="/banking" name="Banking" className="banking" />
-          <HomeTileComponent path="/reactplayground" name="React Playground" className="react-playground" />
-        </div>
+        <HomeTitle>What would you like to do today?</HomeTitle>
+        <HomeTilesWrapper>
+          <HomeTile path="/banking" name="Banking" className="banking" />
+          <HomeTile path="/reactplayground" name="React Playground" className="react-playground" />
+        </HomeTilesWrapper>
       </div>
     );
   }
---
+++ src/home/HomeTile.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { string } from 'prop-types';
+import bankingImage from './images/banking.png';
+import reactImage from './images/react.png';
+import styled, { css } from 'styled-components';
+
+const HomeTileWrapper = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  width: 200px;
+  height: 200px;
+  padding: 5px;
+  margin: 10px;
+  background-color: #fafafa;
+  border: 2px solid #002742;
+  border-radius: 10px;
+  background-size: 80%;
+  background-repeat: no-repeat;
+  background-position: center 10px;
+  font-weight: bold;
+  font-size: 20px;
+
+  ${props => {
+    switch (props.className) {
+      case 'banking': {
+        return bankingMixin;
+      }
+      case 'react-playground': {
+        return reactPlaygroundMixin;
+      }
+      default:
+        break;
+    }
+  }};
+`;
+
+const bankingMixin = css`
+  background-image: url(${bankingImage});
+  background-size: 60%;
+  background-position: center 20px;
+`;
+
+const reactPlaygroundMixin = css`
+  background-image: url(${reactImage});
+  background-size: 110%;
+  background-position: center 0px;
+`;
+
+const HomeTile = ({ path, name, className }) => {
+  return (
+    <Link to={{ pathname: path }}>
+      <HomeTileWrapper className={`${className}`}>
+        <div>{name}</div>
+      </HomeTileWrapper>
+    </Link>
+  );
+};
+
+HomeTile.propTypes = {
+  path: string.isRequired,
+  name: string.isRequired,
+  className: string.isRequired
+};
+
+HomeTile.defaultProps = {};
+
+export default HomeTile;
