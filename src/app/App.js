12ce4fdfe47a08b39d126f7d1fe537a29fa89edd
Laura Meikle <laura.s.meikle@gmail.com>
2018-07-04 08:00:34 +0100

    nested routing

--- src/app/App.js
+++ src/app/App.js
@@ -1,27 +1,21 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import AtmsContainer from '../banking/atms/AtmsContainer';
-import BanksContainer from '../banking/banks/BanksContainer';
-import logo from './logo.svg';
+import BankingApp from '../banking/page/BankingApp';
 import './App.css';
 
 class App extends Component {
-	render()
-	{
-		return (
-			<div className="app">
-				<div className="app-topbar">
-					<img src={logo} className="app-topbar-logo" alt="logo"/>
-				</div>
-				<Router>
-					<Switch>
-						<Route path="/atms" component={AtmsContainer}/>
-						<Route path="/" component={BanksContainer}/>
-					</Switch>
-				</Router>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="app">
+        <Router>
+          <Switch>
+            <Route path="/banking" component={BankingApp} />
+            <Route path="/" component={BankingApp} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
